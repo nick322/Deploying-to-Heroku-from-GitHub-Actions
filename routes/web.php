9464650141094b5dfe547b5737f0d274fa131c35
NickHuang <nick_huang@owlting.com>
2021-03-13 08:54:03 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,20 @@ use Phpfastcache\Helper\Psr16Adapter;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/a', function () {
+// Route::get('/a', function () {
 
-    $instagram = \InstagramScraper\Instagram::withCredentials(new \GuzzleHttp\Client(
-        ['proxy' => 'HTTP://14.207.84.75:8080','defaults' => [
-            'verify' => false
-        ]]
-    ),
-    env('IG_ACCOUNT'),env('IG_PASSWORD') , new Psr16Adapter('Files'));
-    $instagram->login(); 
-    $instagram->saveSession();  
+//     $instagram = \InstagramScraper\Instagram::withCredentials(new \GuzzleHttp\Client(
+//         ['proxy' => 'HTTP://14.207.84.75:8080','defaults' => [
+//             'verify' => false
+//         ]]
+//     ),
+//     env('IG_ACCOUNT'),env('IG_PASSWORD') , new Psr16Adapter('Files'));
+//     $instagram->login(); 
+//     $instagram->saveSession();  
 
-    $nonPrivateAccountMedias = $instagram->getPaginateMediaCommentsByCode('CL6CVqlB7fE', 100 ,null);
+//     $nonPrivateAccountMedias = $instagram->getPaginateMediaCommentsByCode('CL6CVqlB7fE', 100 ,null);
 
-    dd($nonPrivateAccountMedias);
-    echo $nonPrivateAccountMedias[0]->getLink();
+//     dd($nonPrivateAccountMedias);
+//     echo $nonPrivateAccountMedias[0]->getLink();
 
-});
+// });
